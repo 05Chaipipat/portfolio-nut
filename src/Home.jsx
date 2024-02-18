b90d967e9a048128762dd6a7cf9b4eda01a64755
05Chaipipat <150114910+05Chaipipat@users.noreply.github.com>
2024-02-18 13:25:30 +0700

    add

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         alt="profile"
       />
       
-      <h1 className="text-amber-400 font-bold text-4xl">Software-Developer</h1>
+      <h1 className="text-amber-400 font-bold text-4xl p-5">Software-Developer</h1>
       <p className="text-white lg:text-[1rem] pl-5 text-opacity-80  mt-[2rem] lg:w-[40%]">
         Hi, I am <span className="text-amber-400">"Nut"</span> or <span className="text-amber-400">"Chaipipat Khemvaraporn"</span> <br /> Junior Software
         Developer bootscamp cohort6 at Generation Thailand. 
